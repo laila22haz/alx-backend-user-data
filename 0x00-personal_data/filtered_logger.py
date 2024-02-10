23990affbe9102dbfd31b65b564577328aa6b239
laila22haz <hazmirlaila316@gmail.com>
2024-02-11 00:02:21 +0100

    data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,3 +63,24 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
     return mysql.connector.connect(
         user=username, password=password, host=db_host, database=db_name)
+
+
+def main() -> None:
+    """main function"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    log = get_logger()
+    for row in cursor:
+        data = []
+        for desc, value in zip(cursor.description, row):
+            pair = f"{desc[0]}={str(value)}"
+            data.append(pair)
+        row_str = "; ".join(data)
+        log.info(row_str)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
