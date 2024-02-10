8913d10123bd9a3feee3eb8779af4a17906ec8bb
laila22haz <hazmirlaila316@gmail.com>
2024-02-11 00:13:02 +0100

    data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,11 @@ def hash_password(password: str) -> bytes:
     """hash_password function"""
     password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid function"""
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
