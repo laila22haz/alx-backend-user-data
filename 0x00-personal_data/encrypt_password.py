1a143e0d5b000ce3169f42803d1dc88ccb681bcb
laila22haz <hazmirlaila316@gmail.com>
2024-02-11 00:09:42 +0100

    data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,8 +2,8 @@
 """Encrypting passwords log"""
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """hash_password function"""
     password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return password
-
