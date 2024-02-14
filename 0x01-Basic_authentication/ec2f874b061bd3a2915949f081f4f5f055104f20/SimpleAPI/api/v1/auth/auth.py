9ef16f52807679828c0bbb2ebd2357c82291d452
laila22haz <hazmirlaila316@gmail.com>
2024-02-14 01:22:50 +0100

    Let's see

---
+++ 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Auth Class"""
+from flask import request, Flask
+from typing import List, TypeVar
+import os
+
+
+class Auth():
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth function"""
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            check += "/"
+        if check in excluded_paths or path in excluded_paths:
+            return False
+        return True
+
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header function"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user function"""
+        return None
