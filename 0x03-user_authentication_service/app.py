cc5cd980128f99ea63b5a627c6d5b011a7942ce0
laila22haz <hazmirlaila316@gmail.com>
2024-02-21 17:40:07 +0100

    Task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
 """App module
 """
-from flask import Flask
+from flask import Flask, abort, request
 from flask import jsonify
+from auth import Auth
+from sqlalchemy.exc import InvalidRequestError
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def index():
     """Bienvenue"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """users function"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({f"email": {email}, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
