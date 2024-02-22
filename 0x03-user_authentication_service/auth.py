d1ce97d0aa90b1ebe95de2e6020e098983f241c7
laila22haz <hazmirlaila316@gmail.com>
2024-02-22 02:00:46 +0100

    Task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,13 @@ class Auth:
             password_hash = _hash_password(password)
             user = self._db.add_user(email, password_hash)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """valid_login function"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(
+                    password.encode('utf-8'), user.hashed_password)
+        except Exception:
+            return False
