fea3c93b3d23c243f53362c8d5b5af28a75e767b
laila22haz <hazmirlaila316@gmail.com>
2024-02-23 02:34:46 +0100

    task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,8 +64,6 @@ def logout() -> str:
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
     """ GET /profile
-      Return:
-        - message
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
